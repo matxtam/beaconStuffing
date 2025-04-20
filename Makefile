3cf56b8f74d80688371d9a50d4f03c1b6db8ed87
matxtam <hd.reservoir@gmail.com>
2025-04-20 16:43:46 +0800

    clean up

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ CC=clang
 CFLAGS=-Wall -Werror -O3 -o sender_a.out
 LDFLAGS=-lwifi -lpcap
 
-all: sender_a.out
+all: send.o
 	$(CC) $(CFLAGS) ./src/send_clean.c $(LDFLAGS)
 	
 clean:
-	rm sender_a.out 
+	rm send.o 
--- src/send.c
+++ src/send.c
@@ -4,8 +4,8 @@
  * sudo ./send.o
  *
  * Author: matxtam
- * Inspired by: Subodh Saxena 
  */
+
 #include <libwifi.h>
 #include <pcap.h>
 
@@ -34,64 +34,94 @@
 #include<arpa/inet.h>
 
 #include<unistd.h>
+
+const char *ifn = "wlan1";
+
 pcap_dumper_t *filedumper = NULL;
-
-struct ifreq ifreq_c,ifreq_i; // for each ioctl keep diffrent ifreq structure
-
-int sock_raw;
-unsigned char *sendbuff;
-struct libwifi_beacon beacon = {0};
  
-void get_eth_data() {
-	memset(&ifreq_i,0,sizeof(ifreq_i)); // initialize the content of ifreq_i
-	strncpy(ifreq_i.ifr_name,"wlan1",IFNAMSIZ-1);
+int get_if_index(const char *ifname) {
+	int sock = socket(AF_INET,SOCK_DGRAM,0);
+	if(sock < 0){
+		perror("socket");
+		return -1;
+	}
 
-	if((ioctl(sock_raw,SIOCGIFINDEX,&ifreq_i))<0)
-		printf("error in index ioctl reading\n");
+	struct ifreq ifr;
+	strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
 
-	printf("index=%d\n",ifreq_i.ifr_ifindex);
+	if((ioctl(sock, SIOCGIFINDEX, &ifr)) < 0){
+		perror("ioctl");
+		close(sock);
+		return -1;
+	}
+
+	close(sock);
+	printf("index = %d\n",ifr.ifr_ifindex);
+	return ifr.ifr_ifindex;
+}
+
+ 
+int get_if_mac(const char *ifname, unsigned char mac[6]) {
+	// int sock = socket(AF_INET,SOCK_DGRAM,0);
+	struct ifreq ifr;
+	int sock;
+       	sock = socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
+	if(sock < 0){
+		perror("socket");
+		return -1;
+	}
+
+	memset(&ifr,0,sizeof(ifr));
+	strncpy(ifr.ifr_name, "wlan1", IFNAMSIZ-1);
+
+	if((ioctl(sock, SIOCGIFHWADDR, &ifr)) < 0){
+		perror("ioctl");
+		close(sock);
+		return -1;
+	}
+
+	printf("Mac= %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[0]),
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[1]),
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[2]),
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[3]),
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[4]),
+			(unsigned char)(ifr.ifr_hwaddr.sa_data[5]));
+
+	close(sock);
+	memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+	return 0;
 }
 
 
-void create_beacon() {
+void create_beacon(struct libwifi_beacon *refbeacon) {
 	printf("Creating Beacon Frame...\n");
 
+	// TX mac
+	unsigned char mac_tx[6] = {0};
+	if(get_if_mac(ifn, mac_tx) != 0){
+		printf("get mac error");
+	}
 
-	// transmitter mac
-	unsigned char transmitter[6] = {0};
-	
-	printf("Getting mac address of the transmitter...\n");
-	memset(&ifreq_c,0,sizeof(ifreq_c)); // initialize the content of ifreq_c
-	strncpy(ifreq_c.ifr_name,"wlan1",IFNAMSIZ-1);
-
-	if((ioctl(sock_raw,SIOCGIFHWADDR,&ifreq_c))<0)
-		printf("error in SIOCGIFHWADDR ioctl reading");
-
-	printf("Mac= %.2X-%.2X-%.2X-%.2X-%.2X-%.2X\n",
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[0]),
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[1]),
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[2]),
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[3]),
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[4]),
-			(unsigned char)(ifreq_c.ifr_hwaddr.sa_data[5]));
-	transmitter[0] = ifreq_c.ifr_hwaddr.sa_data[0];
-	transmitter[1] = ifreq_c.ifr_hwaddr.sa_data[1];
-	transmitter[2] = ifreq_c.ifr_hwaddr.sa_data[2];
-	transmitter[3] = ifreq_c.ifr_hwaddr.sa_data[3];
-	transmitter[4] = ifreq_c.ifr_hwaddr.sa_data[4];
-	transmitter[5] = ifreq_c.ifr_hwaddr.sa_data[5];
-
-	// receiver mac
-	unsigned char receiver[6] = "\xFF\xFF\xFF\xFF\xFF\xFF";
+	// RX mac
+	unsigned char mac_rx[6] = "\xFF\xFF\xFF\xFF\xFF\xFF";
 
 	// use libwifi to create wifi beacon
-	libwifi_create_beacon(&beacon, receiver, transmitter, transmitter, "libwifi-beacon", 1);
-	libwifi_quick_add_tag(&beacon.tags, TAG_VENDOR_SPECIFIC,
+	libwifi_create_beacon(refbeacon, mac_rx, mac_tx, mac_tx, "libwifi-beacon", 1);
+	libwifi_quick_add_tag(&(refbeacon->tags), TAG_VENDOR_SPECIFIC,
 			(unsigned char *) "libwifi-tag", strlen("libwifi-tag"));
 
 }
 
 void check_beacon(const unsigned char *packet, int len) {
+	// print packet content
+	printf("checking packet...\n");
+	printf("packet content:");
+	for(int i=0; i<len; i++){
+			printf("%c", packet[i]);
+		}
+	printf("\n");
+
     if (len < 24) {  // Minimum beacon frame size
         printf("Packet too short to be a beacon.\n");
         return;
@@ -110,7 +140,7 @@ void check_beacon(const unsigned char *packet, int len) {
         int ssid_len = packet[37];  // SSID length
         if (ssid_len > 0 && ssid_len < 32) {  // Valid SSID length
             printf("SSID: ");
-            for (int i = 0; i < ssid_len; i++) {
+        for (int i=0; i<ssid_len; i++) {
                 printf("%c", packet[38 + i]);
             }
             printf("\n");
@@ -122,136 +152,92 @@ void check_beacon(const unsigned char *packet, int len) {
     }
 }
 
-/*int main()
-{
-	// for pcap
-	pcap_t *handle = NULL;
-	char errbuf[PCAP_ERRBUF_SIZE] = {0};
-    	FILE *pcapfile = NULL;
-   	pcapfile = fopen("beacon.pcap", "w+");
-        if ((handle = pcap_open_dead(DLT_IEEE802_11, BUFSIZ)) == NULL) {
-            fprintf(stderr, "[!] Error opening dead capture (%s)\n", errbuf);
-            exit(EXIT_FAILURE);
-        }
-        if ((filedumper = pcap_dump_fopen(handle, pcapfile)) == NULL) {
-            fprintf(stderr, "[!] Error opening file %s (%s)\n", "beacon.pcap", errbuf);
-            exit(EXIT_FAILURE);
-        }
-
-	// open raw socket
-	// family, type, protocol
-	// sock_raw=socket(AF_PACKET,SOCK_RAW,IPPROTO_RAW);
-	printf("Creating socket...");
-	sock_raw=socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
-	if(sock_raw == -1)
-		printf("error in socket");
-	get_eth_data();
-	// open socket
-	struct sockaddr_ll sadr_ll;
-	sadr_ll.sll_family = AF_PACKET;  // added
-	sadr_ll.sll_protocol = htons(ETH_P_ALL); // added
-	sadr_ll.sll_ifindex = ifreq_i.ifr_ifindex;
-	sadr_ll.sll_halen   = ETH_ALEN;
-	
-	create_beacon();
-
-	size_t buf_len = libwifi_get_beacon_length(&beacon);
-	sendbuff=(unsigned char*)malloc(buf_len);
-	if (sendbuff == NULL)
-		printf("error allocate buffer");
-	memset(sendbuff,0,buf_len);
-	libwifi_dump_beacon(&beacon, sendbuff, buf_len);
-
-	printf("checking packet...\n");
-	check_beacon(sendbuff, buf_len);
-	for(int i=0; i<buf_len; i++){
-		printf("%c", sendbuff[i]);
-	}
-	printf("\n");
-
-	printf("[*] Writing Beacon Frame to pcap\n");
-    	struct pcap_pkthdr pkt_hdr = {0};
-    	struct timeval tv = {0};
-    	pkt_hdr.caplen = buf_len;
-    	pkt_hdr.len = buf_len;
-    	gettimeofday(&tv, NULL);
-    	pkt_hdr.ts = tv;
-    	pcap_dump((unsigned char *) filedumper, &pkt_hdr, sendbuff);
-	pcap_dump_close(filedumper);
-    	pcap_close(handle);
-
-	printf("sending packets...\n");
-	while (1){
-		
-		usleep(500 * 1000);
-		int send_len = sendto(
-				sock_raw, // sockfd: file discriptor of the sending socket
-				sendbuff, // buf: message content
-				buf_len,       // message length
-				0,        // flags
-				(const struct sockaddr*)&sadr_ll, // destination address
-				sizeof(struct sockaddr_ll)        // size of destination address
-		);
-		if(send_len<0) {
-			printf("error in sending...sendlen=%d, errno=%d\n",send_len,errno);
-			return -1;
-		}
-		// printf(".\n");
-
-	}
-	return 0;
-
-}*/
-
 int main()
 {
 
-	printf("Creating socket...");
-	sock_raw=socket(AF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
-	if(sock_raw == -1)
-		printf("error in socket");
-	get_eth_data();
-	
-	create_beacon();
-	if (close(sock_raw) < 0) {
-        perror("Failed to close socket");
-        return -1;
-    }
-
+	// create beacon frame
+	struct libwifi_beacon beacon = {0};
+	create_beacon(&beacon);
 	size_t buf_len = libwifi_get_beacon_length(&beacon);
-	sendbuff=(unsigned char*)malloc(buf_len);
-	if (sendbuff == NULL)
-		printf("error allocate buffer");
+	
+	// copy beacon frame into buffer
+	unsigned char *sendbuff = malloc(buf_len);
+	if (sendbuff == NULL){ printf("error allocate buffer"); }
 	memset(sendbuff,0,buf_len);
 	libwifi_dump_beacon(&beacon, sendbuff, buf_len);
 
-	printf("checking packet...\n");
+	// check packet
 	check_beacon(sendbuff, buf_len);
-	for(int i=0; i<buf_len; i++){
-		printf("%c", sendbuff[i]);
+
+	// create radiotap
+	struct libwifi_radiotap_info rtap_info = {0};
+	rtap_info.present = 0x0000002e;     // Flags, Rate, Channel, dBm Ant Signal
+	rtap_info.channel.flags = 0x00a0;   // CCK, 2.4GHz (for channel 1)
+	rtap_info.channel.freq = 2412;      // Channel 1 (2.4GHz)
+	rtap_info.flags = 0x0000;           // No flags
+	rtap_info.rate = 1;                 // 1 Mbit
+	rtap_info.rate_raw = rtap_info.rate * 2; // 500kb/s increments
+	rtap_info.signal = -20;             // Signal strength in dBm
+
+	char *rtap = malloc(LIBWIFI_MAX_RADIOTAP_LEN);
+	if (rtap == NULL){ printf("error allocate rtap"); }
+	memset(rtap,0,LIBWIFI_MAX_RADIOTAP_LEN);
+	
+	int rtap_len = libwifi_create_radiotap(&rtap_info, rtap);
+	if (rtap_len == -1) {
+		 fprintf(stderr, "Error generating radiotap header\n");
+		 free(rtap);
+		 free(sendbuff);
+		 exit(EXIT_FAILURE);
 	}
-	printf("\n");
 
+	// combine radiotap and beacon frame
+	size_t total_len = rtap_len + buf_len;
+	unsigned char *frame = malloc(total_len);
+	if (frame == NULL) {
+		perror("malloc failed");
+		free(rtap);
+		free(sendbuff);
+		exit(EXIT_FAILURE);
+	}
+	memcpy(frame, rtap, rtap_len);
+	memcpy(frame + rtap_len, sendbuff, buf_len);
 
-	printf("sending packets...\n");
-	pcap_t *handle = NULL;
+	// open a pcap handle
 	char errbuf[PCAP_ERRBUF_SIZE] = {0};
-	char *dev = "wlan1";  // Replace with your network interface
-    	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	pcap_t *handle = pcap_open_live(ifn, BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
-		fprintf(stderr, "Error opening device %s: %s\n", dev, errbuf);
+		fprintf(stderr, "Error opening device %s: %s\n", ifn, errbuf);
 		return -1;
 	}
+
+	// check datalink type
+	int dlt = pcap_datalink(handle);
+	printf("Data Link Type: %s\n", pcap_datalink_val_to_name(dlt));
+	if (dlt != DLT_IEEE802_11_RADIO) {
+    fprintf(stderr, "Warning: Expected DLT_IEEE802_11_RADIO, got %s\n", pcap_datalink_val_to_name(dlt));
+    }
+
+	// send packet
+	printf("sending packets on %s...\n", ifn);
 	while (1){
-		if (pcap_sendpacket(handle, sendbuff, buf_len) != 0) {
+		if (pcap_sendpacket(handle, frame, total_len) != 0) {
 			fprintf(stderr, "Error sending packet: %s\n", pcap_geterr(handle));
+			pcap_close(handle);
+			free(rtap);
+			free(sendbuff);
+			free(frame);
 			return -1;
 		}
 		
 		usleep(200 * 1000);
 
 	}
-    	pcap_close(handle);
-	return 0;
 
+  pcap_close(handle);
+	free(rtap);
+	free(sendbuff);
+	free(frame);
+	
+	return 0;
 }
